e01f4f69d47e1b680682f4761764a0e1c4085f0a
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-09-28 17:21:52 -0600

    improve connections to work correctly

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,17 +26,23 @@ $(document).ready(function() {
     var isPlayer1 = false;
     var isPlayer2 = false;
     
-    database.ref("/player1").on("value", function(snap) {
+    dbPlayer1.on("value", function(snap) {
         console.log(snap.val());
-        if (snap.val()) {
+        if (snap.val() === null) {
+            requestPlayer1();
+        }
+        else {
             $(".player1").empty();
             createPlayerArea();
         }
     });
         
-    database.ref("/player2").on("value", function(snap) {
+    dbPlayer2.on("value", function(snap) {
         console.log(snap.val());
-        if (snap.val()) {
+        if (snap.val() === null) {
+            requestPlayer2();
+        }
+        else {
             $(".player2").empty();
             createPlayerArea();
         }
@@ -48,132 +54,145 @@ $(document).ready(function() {
         if (snap.val()) {
             var con = connectionsRef.push(true);
             con.onDisconnect().remove();
-            requestPlayer();
         }
     });
     
     // add how many people are on the website here
-    connectionsRef.on("value", function(snap) {});
+    connectionsRef.on("value", function(snap) {
+        console.log(snap.numChildren())
+    });
     
     
     // create fields for new players if there arent 2 already
-    function requestPlayer() {
+    function requestPlayer1() {
         database.ref().once("value").then(function(snap) {
-            console.log(" --- DB --- ");
-            console.log(snap);
-            console.log( " --- DB --- ");
-            // console.log("Have player 1? ", snap.val().player1.name, " or have player 2? ", snap.val().player2.names);
-
-            if (!snap.val().havePlayer1) {
-                dbPlayer1.remove();
-                var form = $("<form>");
-                    var labelName = $("<label>")
-                    .addClass("name-label")
-                        .attr("for", "player1-name")
-                        .text("Player 1: ");
-                        var inputName = $("<input>")
-                        .addClass("name-input")
-                        .attr({
-                            id: "player1-name",
-                            type: "text",
-                            placeholder: "Enter your name"
-                        });
-                    var submitBtn = $("<button>")
-                    .addClass("submit-btn")
-                        .attr({
-                            id: "1"
-                        })
-                        .text("Submit");
-                        form.append(labelName, inputName, submitBtn);
-                    $(".player1").append(form);
-                }
-
-                if (!snap.val().havePlayer2) {
-                    dbPlayer2.remove();
+            if (!isPlayer2) {
+                if (snap.val().player1 === undefined) {
+                    console.log("if /player1 doesnt exist");
                     var form = $("<form>");
-                    var labelName = $("<label>")
-                        .addClass("name-lbl")
-                        .attr("for", "player2-name")
-                        .text("Player 2: ");
-                    var inputName = $("<input>")
-                        .addClass("name-input")
-                        .attr({
-                            id: "player2-name",
-                            type: "text",
-                            placeholder: "Enter your name"
-                        });
-                    var submitBtn = $("<button>")
-                        .addClass("submit-btn")
-                        .attr({
-                            id: "2"
-                        })
-                        .text("Submit");
-                    form.append(labelName, inputName, submitBtn);
-                    $(".player2").append(form);
+                        var labelName = $("<label>")
+                        .addClass("name-label")
+                            .attr("for", "player1-name")
+                            .text("Player 1: ");
+                            var inputName = $("<input>")
+                            .addClass("name-input")
+                            .attr({
+                                id: "player1-name",
+                                type: "text",
+                                placeholder: "Enter your name"
+                            });
+                        var submitBtn = $("<button>")
+                            .addClass("submit-btn")
+                            .attr({
+                                id: "submit-1",
+                                value: "1",
+                            })
+                            .text("Submit");
+                            form.append(labelName, inputName, submitBtn);
+                        $(".player1").append(form);
+                    }
+                    $("#submit-1").on("click", addPlayer);
                 }
-                $(".submit-btn").on("click", addPlayer);
             });
-    }
-
-    function addPlayer(event) {
-        event.preventDefault();
-        console.log("prevented?");
-
-        if (this.id == 1) {
-            isPlayer1 = true;
-            dbPlayer1.update({
-                name: $("#player1-name").val(),
-                score: 0
-            });
-            dbPlayer1.onDisconnect().remove();
         }
-        if (this.id == 2) {
-            isPlayer2 = true;
-            dbPlayer2.update({
-                name: $("#player2-name").val(),
-                score: 0
+
+        function requestPlayer2() {
+            database.ref().once("value").then(function(snap) {    
+                if (!isPlayer1) {
+                    if (snap.val().player2 === undefined) {
+                        console.log("if /player2 doesnt exist")
+                        var form = $("<form>");
+                        var labelName = $("<label>")
+                            .addClass("name-lbl")
+                            .attr("for", "player2-name")
+                            .text("Player 2: ");
+                        var inputName = $("<input>")
+                            .addClass("name-input")
+                            .attr({
+                                id: "player2-name",
+                                type: "text",
+                                placeholder: "Enter your name"
+                            });
+                        var submitBtn = $("<button>")
+                            .addClass("submit-btn")
+                            .attr({
+                                id: "submit-2",
+                                value: "2"
+                            })
+                            .text("Submit");
+                        form.append(labelName, inputName, submitBtn);
+                        $(".player2").append(form);
+                    }
+                    $("#submit-2").on("click", addPlayer);
+                }
             });
-            dbPlayer2.onDisconnect().remove();
         }
-        console.log("player 1? ", isPlayer1, " or Player 2? ", isPlayer2);
-    }
-
-
-    // create player area for either player 1 or 2
-    function createPlayerArea() {
-        var nameDiv = $("<div>").addClass("name-div");
-        nameDiv.text(this.name);
-        var btnsDiv = $("<div>").addClass("btns-div");
-        var rockBtn = $("<button>")
-            .addClass("rock")
-            .text("Rock");
-        var paperBtn = $("<button>")
-            .addClass("paper")
-            .text("Paper");
-        var scissorsBtn = $("<button>")
-            .addClass("scissors")
-            .text("Scissors");
-
-        if (isPlayer1) {
-            console.log("buttons! p1")
+        
+        function addPlayer(event) {
+            event.preventDefault();
+            
+            if (this.value == 1) {
+                isPlayer1 = true;
+                var playerName = $("#player1-name").val();
+                console.log("player1 name ", playerName);
+                dbPlayer1.update({
+                    name: playerName,
+                    score: 0
+                });
+                dbPlayer1.onDisconnect().remove();
+            }
+            if (this.value == 2) {
+                isPlayer2 = true;
+                var playerName = $("#player2-name").val();
+                dbPlayer2.update({
+                    name: playerName,
+                    score: 0
+                });
+                dbPlayer2.onDisconnect().remove();
+            }
+            console.log("player 1? ", isPlayer1, " or Player 2? ", isPlayer2);
+        }
+        
+        
+        // create player area for either player 1 or 2
+        function createPlayerArea() {
+            database.ref().once("value").then(function(snap) {
+                var btnsDiv = $("<div>").addClass("btns-div");
+                var rockBtn = $("<button>")
+                .addClass("rock")
+                .text("Rock");
+                var paperBtn = $("<button>")
+                .addClass("paper")
+                .text("Paper");
+                var scissorsBtn = $("<button>")
+                .addClass("scissors")
+                .text("Scissors");
+                
+                if (isPlayer1) {            
+                    var nameDiv = $("<div>").addClass("name-div");
+                    nameDiv.text(snap.val().player1.name);
+                    $(".player1").empty();
+                    $(".player2").html("<div class='waiting'>Waiting on other players!</div>");
+                    btnsDiv.append(nameDiv).append(rockBtn, paperBtn, scissorsBtn);
+                    $(".player1").append(btnsDiv);
+                } else if (isPlayer2) {
+                    var nameDiv = $("<div>").addClass("name-div");
+                    nameDiv.text(snap.val().player2.name);
+                    $(".player2").empty();
+                    $(".player1").html("<div class='waiting'>Waiting on other players!</div>");
+                    btnsDiv.append(nameDiv).append(rockBtn, paperBtn, scissorsBtn);
+                    $(".player2").append(btnsDiv);
+                }
+                
+            });
+        }
+        
+        // clear out the player areas and create a score area for people to watch
+        function createWatcherArea() {
+            console.log("watcher ran");
             $(".player1").empty();
-            $(".player2").html("<div class='waiting'>Waiting on other players!</div>");
-            btnsDiv.append(nameDiv).append(rockBtn, paperBtn, scissorsBtn);
-            $(".player1").append(btnsDiv);
-        } else if (isPlayer2) {
-            console.log("buttons! p2")
             $(".player2").empty();
-            $(".player1").html("<div class='waiting'>Waiting on other players!</div>");
-            btnsDiv.append(nameDiv).append(rockBtn, paperBtn, scissorsBtn);
-            $(".player2").append(btnsDiv);
         }
-
-    }
-
-    // clear out the player areas and create a score area for people to watch
-    function createWatcherArea() {
-        console.log("watcher ran");
-        $(".player1").empty();
-        $(".player2").empty();
-    }
-});
+    });
+    
+  
